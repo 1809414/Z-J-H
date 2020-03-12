d63736b2f03dc4ffda92c41bae65ef56851e3121
WEI <1906382@students.ucreative.ac.uk>
2020-03-12 17:27:33 +0000

    new c++ camera file

---
+++ Source/CppSideScroller/Camera.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppSideScrollerCharacter.h"
+
+#include "Camera.generated.h"
+
+UCLASS()
+class CPPSIDESCROLLER_API ACamera : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION(EditAnywhere, BlueprintReadOnly, Category =Camera)
+		FVector location;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void followingPlayer();
+};
